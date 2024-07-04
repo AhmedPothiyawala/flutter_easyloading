79d2746564d6ca9777c0900810e7a8e3b0a3c259
AhmedPothiyawala <112747757+AhmedPothiyawala@users.noreply.github.com>
2024-07-04 17:50:15 +0530

    Update flutter_easyloading.dart

--- lib/flutter_easyloading.dart
+++ lib/flutter_easyloading.dart
@@ -26,13 +26,14 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:back_button_interceptor/back_button_interceptor.dart';
 
-import 'src/widgets/container.dart';
-import 'src/widgets/progress.dart';
-import 'src/widgets/indicator.dart';
-import 'src/widgets/overlay_entry.dart';
-import 'src/widgets/loading.dart';
-import 'src/animations/animation.dart';
-import 'src/theme.dart';
+import '.src/widgets/container.dart';
+import '.src/widgets/progress.dart';
+import '.src/widgets/indicator.dart';
+import '.src/widgets/overlay_entry.dart';
+import '.src/widgets/loading.dart';
+import '.src/animations/animation.dart';
+import '.src/theme.dart';
+
 
 /// loading style
 enum EasyLoadingStyle {
